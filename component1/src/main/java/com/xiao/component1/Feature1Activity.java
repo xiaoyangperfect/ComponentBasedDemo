62752f0c86c28ad07a60bf7a71c6ff1c31c29605
肖阳 <yangyang.xiao@net263.com>
2019-02-28 10:55:16 +0800

    Initial commit

---
+++ component1/src/main/java/com/xiao/component1/Feature1Activity.java
@@ -0,0 +1,14 @@
+package com.xiao.component1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Feature1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feature1);
+    }
+}
